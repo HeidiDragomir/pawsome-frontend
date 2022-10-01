166b265c99d5e4f5b2309784cf96d9a69a4ed1ab
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-01 20:54:50 +0200

    Create routes for login, register, admin, homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/header/Header'
+import AdminPage from './pages/AdminPage'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
 // eslint-disable-next-line react/function-component-definition
 const App = () => {
-    return <div>Pawsome</div>
+    return (
+        <Router>
+            <div className="container">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<HomePage />} exact />
+                    <Route path="/admin" element={<AdminPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                </Routes>
+            </div>
+        </Router>
+    )
 }
 
 export default App
