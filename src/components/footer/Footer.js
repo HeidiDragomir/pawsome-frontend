146af03d87568ab38cc98d58b8d80929773ee13f
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-01 20:57:41 +0200

    Update header component with links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 // eslint-disable-next-line react/function-component-definition
 const Footer = () => {
     return <div>Footer</div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import { FaSignInAlt, FaSignOutAlt, FaUser, FaAngellist } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 // eslint-disable-next-line react/function-component-definition
 const Header = () => {
-    return <div>Header</div>
+    return (
+        <header className="header">
+            <div className="logo">
+                <Link to="/">Pets Home Logo</Link>
+            </div>
+            <ul>
+                <li>
+                    <Link to="/login">
+                        <FaSignInAlt />
+                        Login
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/register">
+                        <FaUser />
+                        Register
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/admin">
+                        <FaAngellist />
+                        Admin
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <FaSignOutAlt />
+                        Logout
+                    </Link>
+                </li>
+            </ul>
+        </header>
+    )
 }
 
 export default Header
