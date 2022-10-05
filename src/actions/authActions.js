340810757292854a14a5dcd3a5509f046ba880fc
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-05 15:31:45 +0200

    Create authAction and add register action

---
+++ src/actions/authActions.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+import { USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL } from './types'
+
+// eslint-disable-next-line import/prefer-default-export
+export const register = (name, email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_REGISTER_REQUEST,
+        })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+
+        const { data } = await axios.post(
+            `${process.env.REACT_APP_API_URL}/api/users`,
+            { name, email, password },
+            config
+        )
+
+        dispatch({
+            type: USER_REGISTER_SUCCESS,
+            payload: data,
+        })
+
+        // eslint-disable-next-line no-undef
+        localStorage.setItem('userInfo', JSON.stringify(data))
+    } catch (error) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: error.message,
+        })
+    }
+}
