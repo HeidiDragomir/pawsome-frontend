78d907b4884f9ddc95e047bdd7787599cae82354
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-08 18:40:51 +0200

    Create login and logout reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,7 +1,15 @@
-import { USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL } from '../actions/types'
+import {
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL,
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGIN_FAIL,
+    USER_LOGOUT,
+} from '../actions/types'
 
-// eslint-disable-next-line import/prefer-default-export, default-param-last
-const registerReducer = (state = {}, action) => {
+// eslint-disable-next-line default-param-last
+const userRegisterReducer = (state = {}, action) => {
     switch (action.type) {
         case USER_REGISTER_REQUEST:
             return { loading: true }
@@ -9,9 +17,25 @@ const registerReducer = (state = {}, action) => {
             return { loading: false, userInfo: action.payload }
         case USER_REGISTER_FAIL:
             return { loading: false, error: action.payload }
+        case USER_LOGOUT:
+            return {}
         default:
             return state
     }
 }
 
-export default registerReducer
+// eslint-disable-next-line default-param-last
+const userLoginReducer = (state = {}, action) => {
+    switch (action.type) {
+        case USER_LOGIN_REQUEST:
+            return { loading: true }
+        case USER_LOGIN_SUCCESS:
+            return { loading: false, userInfo: action.payload }
+        case USER_LOGIN_FAIL:
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
+
+export { userRegisterReducer, userLoginReducer }
