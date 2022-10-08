219ce3ece885319b46904b5acfc5cf16c18f555b
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-08 18:41:14 +0200

    Create login and logout actions

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,8 +1,16 @@
 import axios from 'axios'
 
-import { USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL } from './types'
+import {
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL,
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGIN_FAIL,
+    USER_LOGOUT,
+} from './types'
 
-// eslint-disable-next-line import/prefer-default-export
+// REGISTER
 export const register = (name, email, password) => async (dispatch) => {
     try {
         dispatch({
@@ -37,4 +45,49 @@ export const register = (name, email, password) => async (dispatch) => {
                     : error.message,
         })
     }
+}
+
+// LOGIN
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST,
+        })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+        const { data } = await axios.post(
+            `${process.env.REACT_APP_API_URL}/api/users/login`,
+            { email, password },
+            config
+        )
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data,
+        })
+
+        // eslint-disable-next-line no-undef
+        localStorage.setItem('userInfo', JSON.stringify(data))
+    } catch (error) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        })
+    }
+}
+
+// LOGOUT
+export const logout = () => (dispatch) => {
+    // eslint-disable-next-line no-undef
+    localStorage.removeItem('userInfo')
+    dispatch({
+        type: USER_LOGOUT,
+    })
 }
