8716822109dafcd00af81434b35f59d8e7cb3f8d
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-12 16:05:01 +0200

    Create pets component

---
+++ src/components/pets/Pets.js
@@ -0,0 +1,36 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Card from '../card/Card'
+import Loader from '../loader/Loader'
+import Message from '../message/Message'
+
+import { listPets } from '../../actions/petActions'
+
+// eslint-disable-next-line react/function-component-definition
+const Pets = () => {
+    const dispatch = useDispatch()
+
+    const petList = useSelector((state) => state.petList)
+    const { loading, error, pets } = petList
+
+    useEffect(() => {
+        dispatch(listPets())
+    }, [dispatch])
+
+    return (
+        <section className="pets-section">
+            <div className="pets-container">
+                {error && <Message variant="danger">{error}</Message>}
+                {loading && <Loader />}
+                <div className="pets">
+                    {pets.map((pet) => (
+                        // eslint-disable-next-line no-underscore-dangle
+                        <Card key={pet._id} pet={pet} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Pets
