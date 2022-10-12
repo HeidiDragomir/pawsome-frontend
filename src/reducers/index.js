ab6e039f164bc062018898c2ae73e1170fa6a958
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-12 16:07:42 +0200

    Add pet reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,23 @@
 import { combineReducers } from 'redux'
-import { userRegisterReducer, userLoginReducer } from './authReducer'
+import { userRegisterReducer, userLoginReducer } from './authReducers'
+import {
+    petDetailsReducer,
+    petListReducer,
+    petCreateReducer,
+    petUpdateReducer,
+    petDeleteReducer,
+    petMyListReducer,
+} from './petReducers'
 
 const rootReducer = combineReducers({
     userRegister: userRegisterReducer,
     userLogin: userLoginReducer,
+    petList: petListReducer,
+    petMyList: petMyListReducer,
+    petInfo: petDetailsReducer,
+    petCreate: petCreateReducer,
+    petUpdate: petUpdateReducer,
+    petDelete: petDeleteReducer,
 })
 
 export default rootReducer
