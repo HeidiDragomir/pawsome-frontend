b9ea3e323bc71dbd801b94801dd88d9f55743f44
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-12 16:04:27 +0200

    Add exceptions for links

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,21 @@
 import { NavDropdown } from 'react-bootstrap'
 import { FaSignInAlt, FaSignOutAlt, FaUser, FaAngellist } from 'react-icons/fa'
-import { useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
 import { logout } from '../../actions/authActions'
 
 // eslint-disable-next-line react/function-component-definition
 const Header = () => {
     const dispatch = useDispatch()
-    // const userLogin = useSelector((state) => state.userLogin)
-    // const { userInfo } = userLogin
+    const navigate = useNavigate()
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
 
     const logoutHandler = () => {
         dispatch(logout())
+        navigate('/')
+        // eslint-disable-next-line no-undef
+        window.location.reload(true)
     }
     return (
         <header className="header">
@@ -20,28 +24,44 @@ const Header = () => {
             </div>
             <ul>
                 <li>
-                    <Link to="/login">
-                        <FaSignInAlt />
-                        Login
-                    </Link>
+                    {!userInfo && (
+                        <Link to="/login">
+                            <FaSignInAlt />
+                            Login
+                        </Link>
+                    )}
                 </li>
                 <li>
-                    <Link to="/register">
-                        <FaUser />
-                        Register
-                    </Link>
+                    {!userInfo && (
+                        <Link to="/register">
+                            <FaUser />
+                            Register
+                        </Link>
+                    )}
                 </li>
                 <li>
-                    <Link to="/admin">
-                        <FaAngellist />
-                        Admin
-                    </Link>
+                    {userInfo && (
+                        <Link to="/profile">
+                            <FaSignInAlt />
+                            My Profile
+                        </Link>
+                    )}
                 </li>
                 <li>
-                    <NavDropdown.Item onClick={logoutHandler}>
-                        <FaSignOutAlt />
-                        Logout
-                    </NavDropdown.Item>
+                    {userInfo && userInfo.isAdmin && (
+                        <Link to="/admin">
+                            <FaAngellist />
+                            Admin
+                        </Link>
+                    )}
+                </li>
+                <li>
+                    {userInfo && (
+                        <NavDropdown.Item onClick={logoutHandler}>
+                            <FaSignOutAlt />
+                            Logout
+                        </NavDropdown.Item>
+                    )}
                 </li>
             </ul>
         </header>
