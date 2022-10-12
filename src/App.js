bae183be613fd25e55a94597005ee9b3a9d629a2
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-12 16:01:35 +0200

    Add routes for pet pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 import AdminPage from './pages/AdminPage'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/auth/LoginPage'
 import RegisterPage from './pages/auth/RegisterPage'
 import ProfilePage from './pages/ProfilePage'
 import AboutMePage from './pages/AboutMePage'
+import PetPage from './pages/PetPage'
+import MyPetsPage from './pages/MyPetsPage'
+import PetEditPage from './pages/PetEditPage'
 
 // eslint-disable-next-line react/function-component-definition
 const App = () => {
@@ -18,11 +22,17 @@ const App = () => {
 
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegisterPage />} />
+
                     <Route path="/profile" element={<ProfilePage />} />
                     <Route path="/profile/aboutme" element={<AboutMePage />} />
+                    <Route path="/profile/pets" element={<MyPetsPage />} />
+                    <Route path="/pet/:id/edit" element={<PetEditPage />} />
+
+                    <Route path="/pet/:id" element={<PetPage />} />
 
                     <Route path="/admin" element={<AdminPage />} />
                 </Routes>
+                <Footer />
             </div>
         </Router>
     )
