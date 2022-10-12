85ec9fea187c03deefbb4e369be5265405d28b0a
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-12 16:05:20 +0200

    Update with pets component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,13 @@
+import Pets from '../components/pets/Pets'
+
 // eslint-disable-next-line react/function-component-definition
 const HomePage = () => {
-    return <div>HomePage</div>
+    return (
+        <div>
+            <h2>HomePage</h2>
+            <Pets />
+        </div>
+    )
 }
 
 export default HomePage
