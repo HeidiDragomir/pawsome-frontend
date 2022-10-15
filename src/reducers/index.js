bb0e045531b0683090c2c1fbe7b23fbbf7317556
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-15 13:19:22 +0200

    Add all new reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,6 +9,42 @@ import {
     petMyListReducer,
 } from './petReducers'
 
+import {
+    eventDetailsReducer,
+    eventListReducer,
+    eventCreateReducer,
+    eventUpdateReducer,
+    eventDeleteReducer,
+    eventMyListReducer,
+} from './eventReducers'
+
+import {
+    donationDetailsReducer,
+    donationListReducer,
+    donationCreateReducer,
+    donationUpdateReducer,
+    donationDeleteReducer,
+    donationMyListReducer,
+} from './donationReducers'
+
+import {
+    volunteerDetailsReducer,
+    volunteerListReducer,
+    volunteerCreateReducer,
+    volunteerUpdateReducer,
+    volunteerDeleteReducer,
+    volunteerMyListReducer,
+} from './volunteerReducers'
+
+import {
+    questionDetailsReducer,
+    questionListReducer,
+    questionCreateReducer,
+    questionUpdateReducer,
+    questionDeleteReducer,
+    questionMyListReducer,
+} from './questionReducers'
+
 const rootReducer = combineReducers({
     userRegister: userRegisterReducer,
     userLogin: userLoginReducer,
@@ -18,6 +54,30 @@ const rootReducer = combineReducers({
     petCreate: petCreateReducer,
     petUpdate: petUpdateReducer,
     petDelete: petDeleteReducer,
+    eventList: eventListReducer,
+    eventMyList: eventMyListReducer,
+    eventInfo: eventDetailsReducer,
+    eventCreate: eventCreateReducer,
+    eventUpdate: eventUpdateReducer,
+    eventDelete: eventDeleteReducer,
+    donationList: donationListReducer,
+    donationMyList: donationMyListReducer,
+    donationInfo: donationDetailsReducer,
+    donationCreate: donationCreateReducer,
+    donationUpdate: donationUpdateReducer,
+    donationDelete: donationDeleteReducer,
+    volunteerList: volunteerListReducer,
+    volunteerMyList: volunteerMyListReducer,
+    volunteerInfo: volunteerDetailsReducer,
+    volunteerCreate: volunteerCreateReducer,
+    volunteerUpdate: volunteerUpdateReducer,
+    volunteerDelete: volunteerDeleteReducer,
+    questionList: questionListReducer,
+    questionMyList: questionMyListReducer,
+    questionInfo: questionDetailsReducer,
+    questionCreate: questionCreateReducer,
+    questionUpdate: questionUpdateReducer,
+    questionDelete: questionDeleteReducer,
 })
 
 export default rootReducer
