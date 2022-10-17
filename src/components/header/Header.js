1f0a153890e275a2d58a759f0086ccb2b05fd389
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-17 21:55:05 +0200

    Design Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,20 @@
-import { NavDropdown } from 'react-bootstrap'
-import { FaSignInAlt, FaSignOutAlt, FaUser, FaAngellist } from 'react-icons/fa'
+import { FaSignInAlt, FaUser, FaSignOutAlt, FaPaw } from 'react-icons/fa'
+import { BiUser } from 'react-icons/bi'
+import { ImProfile } from 'react-icons/im'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import { logout } from '../../actions/authActions'
+import './header.css'
 
 // eslint-disable-next-line react/function-component-definition
 const Header = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
+
     const userLogin = useSelector((state) => state.userLogin)
     const { userInfo } = userLogin
 
@@ -19,51 +26,78 @@ const Header = () => {
     }
     return (
         <header className="header">
-            <div className="logo">
-                <Link to="/">Pets Home Logo</Link>
-            </div>
-            <ul>
-                <li>
-                    {!userInfo && (
-                        <Link to="/login">
-                            <FaSignInAlt />
-                            Login
-                        </Link>
-                    )}
-                </li>
-                <li>
-                    {!userInfo && (
-                        <Link to="/register">
-                            <FaUser />
-                            Register
-                        </Link>
-                    )}
-                </li>
-                <li>
-                    {userInfo && (
-                        <Link to="/profile">
-                            <FaSignInAlt />
-                            My Profile
-                        </Link>
-                    )}
-                </li>
-                <li>
-                    {userInfo && userInfo.isAdmin && (
-                        <Link to="/admin">
-                            <FaAngellist />
-                            Admin
-                        </Link>
-                    )}
-                </li>
-                <li>
-                    {userInfo && (
-                        <NavDropdown.Item onClick={logoutHandler}>
-                            <FaSignOutAlt />
-                            Logout
-                        </NavDropdown.Item>
-                    )}
-                </li>
-            </ul>
+            <nav>
+                <Navbar bg="light" expand="md">
+                    <Container>
+                        <Nav.Link classname="logo-container" href="/">
+                            <div className="logo">
+                                <div>
+                                    <FaPaw className="icon fs-2" />
+                                </div>
+                                <div className="icon-text fs-2">
+                                    Paw<span>some</span>
+                                </div>
+                            </div>
+                        </Nav.Link>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto stroke">
+                                <Nav.Link href="/" className="nav-link fs-5 p-3">
+                                    Home
+                                </Nav.Link>
+                                <Nav.Link href="/pets" className="nav-link fs-5 p-3">
+                                    Pets
+                                </Nav.Link>
+                                <Nav.Link href="/pets" className="nav-link fs-5 p-3">
+                                    Donate
+                                </Nav.Link>
+                                <Nav.Link href="/pets" className="nav-link fs-5 p-3">
+                                    Volunteer
+                                </Nav.Link>
+                                <Nav.Link href="/pets" className="nav-link fs-5 p-3">
+                                    Events
+                                </Nav.Link>
+                                <Nav.Link href="/pets" className="nav-link fs-5 p-3">
+                                    Ask
+                                </Nav.Link>
+                            </Nav>
+
+                            {userInfo ? (
+                                <>
+                                    <BiUser className="icon fs-4" />
+                                    <NavDropdown
+                                        title={`Welcome, ${userInfo.name}`}
+                                        id="username"
+                                        className="pt-2 fs-5"
+                                    >
+                                        <NavDropdown.Item href="/profile">
+                                            <ImProfile className="icon" /> Profile
+                                        </NavDropdown.Item>
+
+                                        {userInfo.isAdmin && (
+                                            <NavDropdown.Item href="/admin">Users</NavDropdown.Item>
+                                        )}
+                                        <NavDropdown.Item onClick={logoutHandler}>
+                                            <FaSignOutAlt className="icon" /> Logout
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                </>
+                            ) : (
+                                <>
+                                    <Nav.Link className="nav-link fs-5 p-2" href="/login">
+                                        <FaSignInAlt className="icon icon-text" />
+                                        Login
+                                    </Nav.Link>
+                                    <Nav.Link className="nav-link fs-5 p-2" href="/register">
+                                        <FaUser className="icon icon-text" />
+                                        Register
+                                    </Nav.Link>
+                                </>
+                            )}
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </nav>
         </header>
     )
 }
