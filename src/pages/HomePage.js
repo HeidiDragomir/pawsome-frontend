e16aecc2e3a54a8349dd537b8ed0cd8ac4f3dd58
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-18 22:02:54 +0200

    Add a new section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,19 +2,54 @@
 // import Donations from '../components/donations/Donations'
 // import Volunteers from '../components/volunteers/Volunteers'
 // import Questions from '../components/questions/Questions'
+import { Button } from 'react-bootstrap'
+import { BiDonateHeart, BiHomeHeart } from 'react-icons/bi'
+import { MdOutlineEventAvailable, MdOutlineLiveHelp } from 'react-icons/md'
+import Hero from '../components/hero/Hero'
 import PetsPage from './PetsPage'
+import './homepage.css'
 
 // eslint-disable-next-line react/function-component-definition
 const HomePage = () => {
     return (
-        <div className="container">
-            <h2>HomePage</h2>
+        <>
+            <Hero />
+            <div className="homepage-section-container">
+                <div className="homepage-cards">
+                    <ul className="homepage-cards-list">
+                        <li className="homepage-card">
+                            <Button href="/donations" type="button" className="btn-homepage-card">
+                                <BiDonateHeart className="icon fs-2" />
+                                <span>Donate</span>
+                            </Button>
+                        </li>
+                        <li className="homepage-card">
+                            <Button href="/volunteers" type="button" className="btn-homepage-card">
+                                <BiHomeHeart className="icon fs-2" />
+                                <span>Volunteer</span>
+                            </Button>
+                        </li>
+                        <li className="homepage-card">
+                            <Button href="/events" type="button" className="btn-homepage-card">
+                                <MdOutlineEventAvailable className="icon fs-2" />
+                                <span>Events</span>
+                            </Button>
+                        </li>
+                        <li className="homepage-card">
+                            <Button href="/questions" type="button" className="btn-homepage-card">
+                                <MdOutlineLiveHelp className="icon fs-2" />
+                                <span>Ask</span>
+                            </Button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
             <PetsPage />
             {/* <Events />
             <Donations />
             <Volunteers />
             <Questions /> */}
-        </div>
+        </>
     )
 }
 
