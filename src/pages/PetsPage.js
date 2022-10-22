a4fb1384a0f378184150feeb3c1042c43fadfbf5
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-22 21:51:51 +0200

    Update with search functionality

--- src/pages/PetsPage.js
+++ src/pages/PetsPage.js
@@ -1,26 +1,32 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
 import CardPet from '../components/card/CardPet'
 import Loader from '../components/loader/Loader'
 import Message from '../components/message/Message'
 import './cardsPage.css'
-
 import { listPets } from '../actions/petActions'
 
 // eslint-disable-next-line react/function-component-definition
 const PetsPage = () => {
     const dispatch = useDispatch()
+    const { keyword } = useParams()
 
     const petList = useSelector((state) => state.petList)
     const { loading, error, pets } = petList
 
     useEffect(() => {
-        dispatch(listPets())
-    }, [dispatch])
+        if (keyword) {
+            dispatch(listPets(keyword))
+        } else {
+            dispatch(listPets())
+        }
+    }, [dispatch, keyword])
 
     return (
         <section className="cards-section">
             <div className="cards-container">
+                {!loading && pets.length === 0 && <h4 className="text-center">Pet not found.</h4>}
                 {error && <Message variant="danger">{error}</Message>}
                 {loading && <Loader />}
 
