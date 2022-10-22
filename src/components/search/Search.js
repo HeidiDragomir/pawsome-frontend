6d670f6db9e3ecbe2d8056c071cc0d8862c018e5
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-22 21:50:38 +0200

    Create search component

---
+++ src/components/search/Search.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import './search.css'
+
+// eslint-disable-next-line react/function-component-definition
+const Search = () => {
+    const [keyword, setKeyword] = useState('')
+
+    const navigate = useNavigate()
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        if (keyword.trim()) {
+            navigate(`/search/${keyword}`)
+        } else {
+            navigate('/')
+        }
+    }
+
+    return (
+        // eslint-disable-next-line react/jsx-no-useless-fragment
+        <>
+            <form onSubmit={submitHandler}>
+                <input
+                    className="navbar-search"
+                    type="text"
+                    name="q"
+                    onChange={(e) => setKeyword(e.target.value)}
+                    // eslint-disable-next-line no-return-assign
+                    onBlur={(e) => (e.target.value = '')}
+                />
+            </form>
+        </>
+    )
+}
+
+export default Search
