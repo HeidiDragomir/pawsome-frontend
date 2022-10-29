f3bf72b70385fc11eac21bef9290709d5fd19a72
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-29 07:29:02 +0200

    Fix height problem and loading message when nothin

--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -20,9 +20,7 @@ const EventsPage = () => {
     return (
         <section className="cards-section">
             <div className="cards-container">
-                {!loading && events.length === 0 && (
-                    <h4 className="text-center">Event not found.</h4>
-                )}
+                {!loading && events.length === 0 && <h4 className="text-center">Nothing found.</h4>}
                 {error && <Message variant="danger">{error}</Message>}
                 {loading && <Loader />}
 
