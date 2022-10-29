a15b5aa2c6252a573ccea4be678ef3c2305e48f9
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-29 23:15:28 +0200

    Create page not found

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/function-component-definition */
+import '../index.css'
+
+const PageNotFound = () => {
+    return (
+        <div className="page-not-found d-flex align-items-center justify-content-center">
+            <h1>Page not found</h1>
+        </div>
+    )
+}
+
+export default PageNotFound
